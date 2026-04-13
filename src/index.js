0082f2efdb95bac6076b9855e886b410667a7680
Joel Wise <joelwise.ndungu@student.moringaschool.com>
2026-04-13 14:50:36 +0300

    My initial commit for this lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  /* your code here */
+
+
+const form = document.getElementById("create-task-form")
+const tasklist = document.getElementById("tasks");
+
+form.addEventListener("submit", (event)=>{
+  event.preventDefault();
+
+const task = document.getElementById("new-task-description").value;
+
+buildToDo(task)
 });
+
+
+function buildToDo(task){
+  const li = document.createElement("li")
+  li.textContent = task
+
+  tasklist.append(li)
+
+
+}
+});
+
